d4dc29289d5c13b32e10123de54e0b074f35d598
McDriz <McDriz@McDrizs-Mac-mini.local>
2014-11-12 17:06:43 -0700

    Just fined tuned a few things.  Still pretty sloppy tho.

--- Assets/Scripts/Decorator/MysteryBox.cs
+++ Assets/Scripts/Decorator/MysteryBox.cs
@@ -3,11 +3,25 @@ using System.Collections;
 
 public class MysteryBox : PlayerDecorator {
 
+
+
 	public MysteryBox(PlayerComponent player)
 		: base (player)
 	{
-		//logic for random trait here.  Not yet implemented
-		ConcretePlayer.cash += 25;	
+		int rand = Random.Range (0, 5);
+
+		if (rand == 0)
+			ConcretePlayer.maxHealth += 25;
+		else if (rand == 1)
+			ConcretePlayer.cash += 25;
+		else if (rand == 2)
+			ConcretePlayer.damage += 25;
+		else if (rand == 3)
+			ConcretePlayer.speed += 25;
+		else if (rand == 4)
+			ConcretePlayer.range += 25;
+		else if (rand == 5)
+			ConcretePlayer.armour += 25;
 	}
 	
 	public override void Special()
